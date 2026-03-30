90c0f1d7075ac843ca00a7b9b791d3895e581e89
JowelislamHabib <jowel@bintofajjal.com>
2026-03-30 22:02:44 +0600

    feat: Pricing Section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import GetStarted from "./components/GetStarted/GetStarted";
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
+import Pricing from "./components/Pricing/Pricing";
 import AllProducts from "./components/Products/AllProducts";
 import Stats from "./components/Stats/Stats";
 
@@ -18,6 +19,8 @@ function App() {
         <AllProducts />
 
         <GetStarted />
+
+        <Pricing />
       </div>
     </>
   );
